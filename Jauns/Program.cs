10809b2c5143150f47dcc0031a5878188d393778
berkelaura <berke.laura@gmail.com>
2022-01-12 08:39:42 +0200

    Add project files.

---
+++ Jauns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jauns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveiki no Lauras\n")
+        }
+    }
+}
